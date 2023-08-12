3fc44ce2822010948d5ac11a06a6f4597c0d01ab
Brad <bradcochi@gmail.com>
2023-08-12 11:16:27 -0600

    initial social media and repository icons added

---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+
+const Home = () => {
+  return (
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p>Hi, my name is</p>
+        <h1>Brad Cochi</h1>
+        <h2>I'm a Full Stack Developer,</h2>
+        <p>
+          I'm a full-stack developer specializing in building (and occasionally
+          designing) exceptional digital experiences. Currently, I'm focused on
+          building responsive full-stack web applications.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
