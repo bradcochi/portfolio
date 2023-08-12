56505b3a3ad749b47d2a0ec4d8eacbf7d20673cb
Brad <bradcochi@gmail.com>
2023-08-12 10:12:18 -0600

    logs uploaded, menu and hamburger added to navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Logo from "../assets/logo.png";
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div>
+        <img src={Logo} alt="Logo" style={{ width: "50px" }} />
+      </div>
+
+      {/* Menu */}
+      <ul className="hidden md:flex">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+
+      {/* Hamburger */}
+      <div className="md:hidden">
+        <FaBars />
+      </div>
+
+      {/* Mobile menu */}
+      <ul className="hidden">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+
+      {/* Social icons */}
+      <div className="hidden"></div>
+    </div>
+  );
+};
+
+export default Navbar;
