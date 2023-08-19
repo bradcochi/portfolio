e9a03d6d10772677e5a3f48fe823fc69eb67e5c7
Brad <bradcochi@gmail.com>
2023-08-19 16:35:05 -0600

    updated Skills component with logos

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,9 +4,13 @@ import CSS from "../assets/css.png";
 import JavaScript from "../assets/javascript.png";
 import Python from "../assets/python.png";
 import ReactImg from "../assets/react.png";
-import Node from "../assets/node.png";
+import Django from "../assets/django.png";
+import FastAPI from "../assets/fastapi.png";
 import Tailwind from "../assets/tailwind.png";
+import Bootstrap from "../assets/bootstrap.png";
 import Mongo from "../assets/mongo.png";
+import PostgreSQL from "../assets/postgresql.png";
+import Docker from "../assets/docker.png";
 
 const Skills = () => {
   return (
@@ -24,14 +28,6 @@ const Skills = () => {
         </div>
         <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
-            <p className="my-4">HTML</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={CSS} alt="CSS icon" />
-            <p className="my-4">CSS</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img
               className="w-20 mx-auto"
               src={JavaScript}
@@ -44,20 +40,53 @@ const Skills = () => {
             <p className="my-4">PYTHON</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
+            <p className="my-4">HTML</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={CSS} alt="CSS icon" />
+            <p className="my-4">CSS</p>
+          </div>
+
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={ReactImg} alt="React icon" />
             <p className="my-4">REACT</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Node} alt="Node JS icon" />
-            <p className="my-4">NODE JS</p>
+            <img className="w-20 mx-auto" src={FastAPI} alt="FastAPI icon" />
+            <p className="my-4">FASTAPI</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Tailwind} alt="HTML icon" />
+            <img className="w-20 mx-auto" src={Django} alt="Django icon" />
+            <p className="my-4">DJANGO</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Tailwind} alt="Tailwind icon" />
             <p className="my-4">TAILWIND</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img
+              className="w-20 mx-auto"
+              src={Bootstrap}
+              alt="Bootstrap icon"
+            />
+            <p className="my-4">BOOTSTRAP</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={Mongo} alt="Mongo DB icon" />
             <p className="my-4">MONGO DB</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img
+              className="w-20 mx-auto"
+              src={PostgreSQL}
+              alt="PostgreSQL icon"
+            />
+            <p className="my-4">POSTGRESQL</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Docker} alt="Docker icon" />
+            <p className="my-4">DOCKER</p>
           </div>
         </div>
       </div>
