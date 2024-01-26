07b4631997104daab0eb39148fe7eba8bf9cd284
Brad <bradcochi@gmail.com>
2024-01-26 15:08:39 -0700

    changed url for NPDB repo

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import NPDBImg from "../assets/projects/NPDBImg.png";
+import CHSAASeeding from "../assets/projects/CHSAASeeding.png";
 
 const Work = () => {
   return (
@@ -18,6 +19,37 @@ const Work = () => {
         {/* Container */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${NPDBImg})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-[#eaebed] track-wider">
+                React JS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://saramargs.gitlab.io/npdt/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-[#eaebed] text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/bradcochi/national-parks-database"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-[#eaebed] text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
           <div
             style={{ backgroundImage: `url(${NPDBImg})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
